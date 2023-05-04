3376d45298f15d44f2d39741c984ab50833bdaca
DaniellyCN <89779359+DaniellyCN@users.noreply.github.com>
2023-05-04 10:04:48 -0300

    bugfix de diretorio

--- src/main/java/com/example/SecondaryController.java
+++ src/main/java/com/example/SecondaryController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.controller to javafx.fxml;
-    exports com.controller;
+    opens com.example to javafx.fxml;
+    exports com.example;
 }
